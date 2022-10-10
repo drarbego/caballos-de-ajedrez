c9e458435ddf0e57498f5393ecad58c5f8099365
Edgar A. García <drarbego@gmail.com>
2022-10-10 00:56:26 -0500

    improve tile finding in enemies

--- Enemy.gd
+++ Enemy.gd
@@ -2,7 +2,6 @@ extends Piece
 
 class_name Enemy
 
-var ORIGIN = Vector2(32, 32)
 var dir = Vector2.ZERO
 var target = null
 
@@ -15,13 +14,40 @@ func set_dir(new_dir: Vector2):
 func set_target(new_target: Piece):
 	self.target = new_target
 
-func _draw():
-	var tip = ORIGIN + (self.dir * 64)
-	draw_line(ORIGIN, tip, Color.black, 3.0)
-	var left = tip + ((ORIGIN - tip).normalized() * 16).rotated(deg2rad(30))
-	draw_line(tip, left, Color.red, 3.0)
-	var right = tip + ((ORIGIN - tip).normalized() * 20).rotated(deg2rad(-30))
-	draw_line(tip, right, Color.red, 3.0)
+func get_class():
+	return "Enemy"
 
-func _process(delta):
-	update()
+func move_to_next_tile():
+	var next_tile = self.board.get_tile(self.tile.board_pos + self.dir)
+
+	if not next_tile:
+		return
+
+	if not self.can_move(next_tile.board_pos):
+		return
+
+	if next_tile.current_piece and next_tile.current_piece.get_class() == self.get_class():
+		return
+
+	if next_tile.current_piece and next_tile.current_piece is PlayablePiece:
+		next_tile.current_piece.on_clash(self)
+			 
+	next_tile.set_piece(self)
+
+func update_direction():
+	var target_pos = self.target.tile.board_pos
+
+	var shortest_dist = INF
+	var resulting_dir = Vector2.ZERO
+
+	for d in [Vector2.UP, Vector2.DOWN, Vector2.LEFT, Vector2.RIGHT]:
+		var neighbor_tile = self.tile.get_neighbor_tile_at(d)
+		if not neighbor_tile:
+			continue
+		var length_vector = target_pos - neighbor_tile.board_pos
+		if length_vector.length() < shortest_dist:
+			if not neighbor_tile.has_enemy() and not neighbor_tile.has_content():
+				shortest_dist = length_vector.length()
+				resulting_dir = d
+
+	self.set_dir(resulting_dir)
